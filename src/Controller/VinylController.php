18af518042998dc65b6e7b9410115060c0120ad8
Hadilll11 <bendoudouhadil@gmail.com>
2024-04-29 09:48:27 +0200

    Custom Entity Methods & Twig Magic

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\VinylMixRepository;
+use App\Service\MixRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,7 +12,8 @@ use function Symfony\Component\String\u;
 class VinylController extends AbstractController
 {
     public function __construct(
-        private bool $isDebug
+        private bool $isDebug,
+        private VinylMixRepository $mixRepository
     ) {
     }
 
@@ -37,7 +39,9 @@ class VinylController extends AbstractController
     public function browse(VinylMixRepository $mixRepository, string $slug = null): Response
     {
         $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
-        $mixes = $mixRepository->findAll();
+
+        $mixes = $mixRepository->findBy([], ['votes' => 'DESC']);
+
         return $this->render('vinyl/browse.html.twig', [
             'genre' => $genre,
             'mixes' => $mixes,
